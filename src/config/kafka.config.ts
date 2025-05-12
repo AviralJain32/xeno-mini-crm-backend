7b30fc12a3406d551460754f86a688556e5ee4e6
Aviral Jain <aviral2853@gmail.com>
2025-05-12 09:08:12 +0530

    fix: kafka errors fix

--- src/config/kafka.config.ts
+++ src/config/kafka.config.ts
@@ -3,8 +3,10 @@ import { Kafka } from 'kafkajs';
 dotenv.config();
 
 export const kafka = new Kafka({
+  // clientId:'xeno-app',
+  // brokers: ['localhost:9092'],
   clientId: process.env.KAFKA_CLIENT_ID || 'xeno-app',
-  brokers: [process.env.KAFKA_BROKERS || 'localhost:9093'],
+  brokers: [process.env.KAFKA_BROKERS || 'localhost:9092'],
   ssl: true,
   sasl: {
     mechanism: 'plain',
